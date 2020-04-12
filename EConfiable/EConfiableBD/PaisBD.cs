c7d51317dc9a4743ec54c8a2abd44b4950d44411
malom <malom@LAPTOP-HB5BBVVC>
2020-04-12 14:45:58 -0500

    Actualización capa de datos Pais

---
+++ EConfiable/EConfiableBD/PaisBD.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EConfiableBD
+{
+    // ===========================================================================
+    /// <summary>
+    /// Author     : MARCO ANTONIO MARTINEZ LOPEZ
+    /// Create date: Apr 12 2020  2:43PM
+    /// Procedimiento almacenado que genera Script de Clases BD de Tablas
+    /// CLASE CONSTRUCTOR, INSERT, UPDATE, DELETE Y SELECT de tablas de Bases de Datos
+    /// </summary>
+
+    public class PaisBD
+    {
+
+        EConfiableEntities BD = new EConfiableEntities();
+
+
+        // ******************* = Costructor = ******************************
+        // Declaración de variables
+        string pai_codigo;
+        string pai_nombre;
+        string pai_codigointernacional;
+        bool pai_predeterminado;
+
+        // Deficinción de propiedades
+        public string Pai_codigo { get; set; }
+        public string Pai_nombre { get; set; }
+        public string Pai_codigointernacional { get; set; }
+        public bool Pai_predeterminado { get; set; }
+
+        public PaisBD()
+        {
+            pai_codigo = "";
+            pai_nombre = "";
+            pai_codigointernacional = "";
+            pai_predeterminado = true;
+        }
+        // ===========================================================================
+        /// <summary>
+        /// Author     : MARCO ANTONIO MARTINEZ LOPEZ
+        /// Create date: Apr 12 2020  2:43PM
+        /// Método que permite insertar en la tabla Pais capa BD
+        /// </summary>
+        // ******************* = Método Insert = ******************************
+        public void Pais_Insert(string pai_codigo, string pai_nombre, string pai_codigointernacional, bool pai_predeterminado)
+        {
+            try
+            {
+                BD.Pais_Insert(pai_codigo, pai_nombre, pai_codigointernacional, pai_predeterminado);
+                BD.SaveChanges();
+            }
+            catch (Exception Ex)
+            {
+                throw (Ex);
+            }
+        }
+        // ===========================================================================
+        /// <summary>
+        /// Author     : MARCO ANTONIO MARTINEZ LOPEZ
+        /// Create date: Apr 12 2020  2:43PM
+        /// Método que permite actualizar en la tabla Pais capa BD
+        /// </summary>
+        // ******************* = Método Update = ******************************
+        public void Pais_Update(string pai_codigo, string pai_nombre, string pai_codigointernacional, bool pai_predeterminado)
+        {
+            try
+            {
+                BD.Pais_Update(pai_codigo, pai_nombre, pai_codigointernacional, pai_predeterminado);
+                BD.SaveChanges();
+            }
+            catch (Exception Ex)
+            {
+                throw (Ex);
+            }
+        }
+        // ===========================================================================
+        /// <summary>
+        /// Author     : MARCO ANTONIO MARTINEZ LOPEZ
+        /// Create date: Apr 12 2020  2:43PM
+        /// Método que permite eliminar en la tabla Pais capa BD
+        /// </summary>
+        // ******************* = Método Delete = ******************************
+        public void Pais_Delete(string pai_codigo)
+        {
+            try
+            {
+                BD.Pais_Delete(pai_codigo);
+                BD.SaveChanges();
+            }
+            catch (Exception Ex)
+            {
+                throw (Ex);
+            }
+        }
+        // ===========================================================================
+        /// <summary>
+        /// Author     : MARCO ANTONIO MARTINEZ LOPEZ
+        /// Create date: Apr 12 2020  2:43PM
+        /// Método que permite listar de la tabla Pais capa BD
+        /// </summary>
+        // ******************* = Método List = ******************************
+        public List<Pais_List_Result> Pais_List(string pai_codigo)
+        {
+            try
+            {
+                var Pais = (from TPais in BD.Pais_List(pai_codigo) select TPais).ToList();
+                return Pais;
+            }
+            catch (Exception Ex)
+            {
+                throw (Ex);
+            }
+        }
+    }
+    //FIN DE LA CLASE
+}
---
+++ EConfiable/EConfiablePRC/PaisPRC.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EConfiableBD;
+
+namespace EConfiablePRC
+{
+    // ===========================================================================
+    /// <summary>
+    /// Author     : MARCO ANTONIO MARTINEZ LOPEZ
+    /// Create date: Apr 12 2020  2:44PM
+    /// Procedimiento almacenado que genera Script de Clases PRC de la capa BD
+    /// CLASE CONSTRUCTOR, INSERT, UPDATE, DELETE Y SELECT de tablas de Bases de Datos
+    /// </summary>
+
+    public class PaisPRC
+    {
+
+        PaisBD BDPais = new PaisBD();
+
+
+        // ******************* = Costructor = ******************************
+        // Declaración de variables
+        string pai_codigo;
+        string pai_nombre;
+        string pai_codigointernacional;
+        bool pai_predeterminado;
+
+        // Deficinción de propiedades
+        public string Pai_codigo { get; set; }
+        public string Pai_nombre { get; set; }
+        public string Pai_codigointernacional { get; set; }
+        public bool Pai_predeterminado { get; set; }
+
+        public PaisPRC()
+        {
+            pai_codigo = "";
+            pai_nombre = "";
+            pai_codigointernacional = "";
+            pai_predeterminado = true;
+        }
+        // ===========================================================================
+        /// <summary>
+        /// Author     : MARCO ANTONIO MARTINEZ LOPEZ
+        /// Create date: Apr 12 2020  2:44PM
+        /// Método que permite insertar en la tabla Pais capa PRC
+        /// </summary>
+        // ******************* = Método Insert = ******************************
+        public void Pais_Insert(string pai_codigo, string pai_nombre, string pai_codigointernacional, bool pai_predeterminado)
+        {
+            try
+            {
+                BDPais.Pais_Insert(pai_codigo, pai_nombre, pai_codigointernacional, pai_predeterminado);
+            }
+            catch (Exception Ex)
+            {
+                throw (Ex);
+            }
+        }
+        // ===========================================================================
+        /// <summary>
+        /// Author     : MARCO ANTONIO MARTINEZ LOPEZ
+        /// Create date: Apr 12 2020  2:44PM
+        /// Método que permite actualizar en la tabla Pais capa PRC
+        /// </summary>
+        // ******************* = Método Update = ******************************
+        public void Pais_Update(string pai_codigo, string pai_nombre, string pai_codigointernacional, bool pai_predeterminado)
+        {
+            try
+            {
+                BDPais.Pais_Update(pai_codigo, pai_nombre, pai_codigointernacional, pai_predeterminado);
+            }
+            catch (Exception Ex)
+            {
+                throw (Ex);
+            }
+        }
+        // ===========================================================================
+        /// <summary>
+        /// Author     : MARCO ANTONIO MARTINEZ LOPEZ
+        /// Create date: Apr 12 2020  2:44PM
+        /// Método que permite eliminar en la tabla Pais capa PRC
+        /// </summary>
+        // ******************* = Método Delete = ******************************
+        public void Pais_Delete(string pai_codigo)
+        {
+            try
+            {
+                BDPais.Pais_Delete(pai_codigo);
+            }
+            catch (Exception Ex)
+            {
+                throw (Ex);
+            }
+        }
+        // ===========================================================================
+        /// <summary>
+        /// Author     : MARCO ANTONIO MARTINEZ LOPEZ
+        /// Create date: Apr 12 2020  2:44PM
+        /// Método que permite listar de la tabla Pais capa PRC
+        /// </summary>
+        // ******************* = Método List = ******************************
+        public List<Pais_List_Result> Pais_List(string pai_codigo)
+        {
+            try
+            {
+                var Pais = (from TPais in BDPais.Pais_List(pai_codigo) select TPais).ToList();
+                return Pais;
+            }
+            catch (Exception Ex)
+            {
+                throw (Ex);
+            }
+        }
+    }
+    //FIN DE LA CLASE
+}
