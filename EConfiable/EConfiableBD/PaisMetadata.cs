ec3073e158b160a93c3f3a36f1c5855dfaf0fa42
malom <malom@LAPTOP-HB5BBVVC>
2020-04-14 15:43:38 -0500

    Actualizar Proyecto

---
+++ EConfiable/EConfiableBD/PaisMetadata.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EConfiableBD
+{
+    [MetadataType(typeof(Pais.Metadata))]
+    public partial class Pais
+    {
+        sealed class Metadata
+        {
+            [DisplayName("Codigo")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(2)]
+            public string pai_codigo { get; set; }
+
+            [DisplayName("Nombre")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(255)]
+            public string pai_nombre { get; set; }
+
+            [DisplayName("CodigoInternacional")]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            [StringLength(2)]
+            public string pai_codigointernacional { get; set; }
+
+            [DisplayName("Predeterminado")]
+            [Required(ErrorMessage = "Debe ingresar el {0}")]
+            public Nullable<bool> pai_predeterminado { get; set; }
+        }
+
+
+    }
+}
---
+++ EConfiable/EConfiableWEB/Controllers/PaisController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EConfiableBD;
+using EConfiablePRC;
+
+
+namespace EConfiableWEB.Controllers
+{
+    public class PaisController : Controller
+    {
+
+        PaisBD db = new PaisBD();
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// Obtener el listado de la tabla Pais
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetData()
+        {
+            List<Pais_List_Result> TPais = db.Pais_List(null).ToList();
+            return Json(new { data = TPais }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
