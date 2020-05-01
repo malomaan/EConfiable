a80e48e6da31c136f979098d3b56c354e5b5cfab
malom <malom@LAPTOP-KGH25DSQ>
2020-05-01 12:36:04 -0500

    Actualizacion de componente Empresa

--- EConfiable/EConfiableWEB/Controllers/EmpresaController.cs
+++ EConfiable/EConfiableWEB/Controllers/EmpresaController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EConfiableBD;
+using EConfiablePRC;
 
 namespace EConfiableWEB.Controllers
 {
@@ -11,6 +13,15 @@ namespace EConfiableWEB.Controllers
         // GET: Empresa
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            CiudadPRC db3 = new CiudadPRC();
+            List<Ciudad_List_Result> LCiudad = new List<Ciudad_List_Result>();                     
+            LCiudad = db3.Ciudad_List(null).ToList();
+            ViewBag.ciu_codigo = new SelectList(LCiudad, "ciu_codigo", "ciu_nombre");
             return View();
         }
     }
